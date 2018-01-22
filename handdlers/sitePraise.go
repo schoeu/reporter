ca0fab6a4e9df95c34fbe6de2d1946c06da4f7ec
schoeu <schoeu89@gmail.com>
2018-01-22 14:40:45 +0800

    add totle sites flow.

--- handdlers/sitePraise.go
+++ handdlers/sitePraise.go
@@ -1,7 +1,6 @@
 package handdlers
 
 import (
-	"time"
 	"database/sql"
 	"fmt"
 	"sync"
@@ -25,19 +24,24 @@ func SitePraise(db *sql.DB, date string) {
 	err = rows.Err()
 	autils.ErrHadle(err)
 	defer rows.Close()
-	fmt.Println(len(newSites))
-	siteFlow(db, newSites, first, last)
-}
 
-func siteFlow(db *sql.DB, sites []string, first time.Time, last time.Time) {
-	var count int
 	firstDateStr := autils.GetCurrentDate(first)
 	lastDateStr := autils.GetCurrentDate(last)
+
+	newSiteFlow := siteFlow(db, newSites, firstDateStr, lastDateStr)
+	total := getTotalFlow(db, firstDateStr, lastDateStr)
+
+	fmt.Println(newSiteFlow, total)
+}
+
+func siteFlow(db *sql.DB, sites []string, first, last string) int {
+	var count int
+	
 	var mutex sync.Mutex
 
 	for _, v := range sites {
 		mutex.Lock()
-		sqlStr := "select ceil(avg(pv)) from site_detail where domain = '"+v+"' and date >= '"+ firstDateStr +"' and date <= '"+ lastDateStr +"'"
+		sqlStr := "select ceil(avg(pv)) from site_detail where domain = '"+v+"' and date >= '"+ first +"' and date <= '"+ last +"'"
 		rows, err := db.Query(sqlStr)
 		var pv int
 		for rows.Next() {
@@ -52,6 +56,20 @@ func siteFlow(db *sql.DB, sites []string, first time.Time, last time.Time) {
 		defer rows.Close()
 		fmt.Println(count)
 		mutex.Unlock()
-
 	}
+	return count
+}
+
+func getTotalFlow(db *sql.DB, first, last string) int {
+	sqlStr := "select ceil(avg(pv)) from site_detail where domain = '总和' and date >= '"+ first +"' and date <= '"+ last +"'"
+	rows, err := db.Query(sqlStr)
+	var total int
+	for rows.Next() {
+		err := rows.Scan(&total)
+		autils.ErrHadle(err)
+	}
+	err = rows.Err()
+	autils.ErrHadle(err)
+	defer rows.Close()
+	return total
 }

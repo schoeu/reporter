a8bf569445561aeffc1e39abd7113d2af7d3f28f
schoeu <schoeu89@gmail.com>
2018-01-22 21:05:22 +0800

    add handdle type.

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ import (
 
 func main() {
 	date := ""
+	aType := 1
+
 	flag.StringVar(&date, "date", "", "分析数据的日期, 默认当前日期（yyyy-MM-dd）")
+	flag.IntVar(&aType, "type", 1, "分析数据类型")
 	flag.Parse()
 
 	if date == "" {
@@ -19,7 +22,13 @@ func main() {
 
 	db := autils.OpenDb("postgres", config.PQFlowUrl)
 	//db := autils.OpenDb("postgres", config.PQTestUrl)
+
+	if aType == 1 {
+		// 增长计算
+		handdlers.SitePraise(db, date)
+	} else if aType == 2 {
+		// TOP站点计算
+		handdlers.TopSites(db, date)
+	}
 	defer db.Close()
-	// 增长计算
-	handdlers.SitePraise(db, date)
 }
